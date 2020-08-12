49303a57ed7416202b80e62b00969794282f9c20
AniketPatel148 <62793287+AniketPatel148@users.noreply.github.com>
2020-08-12 23:01:07 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import "firebase/firestore";
 import "firebase/auth";
 
 let firebaseConfig = {
-	apiKey: "AIzaSyAFlMGU3YEZ5AGn_xCzXat7lbr3gq0P1gY",
-	authDomain: "sahdev-345af.firebaseapp.com",
-	databaseURL: "https://sahdev-345af.firebaseio.com",
-	projectId: "sahdev-345af",
-	storageBucket: "sahdev-345af.appspot.com",
-	messagingSenderId: "685537054731",
-	appId: "1:685537054731:web:e81211e34f91d4bb01f70e",
-	measurementId: "G-HD34HY4BNQ",
+	apiKey: ,
+	authDomain:,
+	databaseURL: ,
+	projectId:,
+	storageBucket: ,
+	messagingSenderId:,
+	appId: ,
+	measurementId: ,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
